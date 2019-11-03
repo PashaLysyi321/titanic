520b319ef0e451ead857d2ef791bbb69c202bb08
Pasha <54781534+PashaLysyi321@users.noreply.github.com>
2019-11-03 13:22:21 +0200

    commit 1 ( increase acc to 0.76555)

--- kaggle.py
+++ kaggle.py
@@ -1,35 +1,43 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
 
-data = pd.read_csv('train.csv', sep=',')
-test = pd.read_csv('test.csv', sep=',')
-idi = test['PassengerId'].valuesMetric
-Your score is the percentage of passengers you correctly predict. This is known as accuracy.
+data = pd.read_csv('C:/Users/lysyi/Desktop/titanic/train.csv', sep=',')
+test = pd.read_csv('C:/Users/lysyi/Desktop/titanic/test.csv', sep=',')
+idi = test['PassengerId'].values
 
-Submission File Format
-You should submit a csv file with exactly 418 entries plus a header row. Your submission will show an error if you have extra columns (beyond PassengerId and Survived) or rows.
-
-The file should have exactly 2 columns:
 
 data.drop(['PassengerId', 'Cabin','Ticket','Name'], axis=1, inplace=True)
 data = data.fillna(value = {'Age' : 23.79929, 'Embarked' : 'S','Fare' : 1000})
-y_train = data['Survived'].values
+y = data['Survived'].values
 data.drop(['Survived'], axis=1, inplace=True)
-
 data = pd.get_dummies(data)
-x_train = data
+data.drop(['Embarked_S','Embarked_Q','Embarked_C', 'Parch', 'Fare'], axis=1, inplace=True)
 
-logReg = LogisticRegression(solver = 'lbfgs',penalty='l2')
-logReg.fit(x_train, y_train)
+pipe = Pipeline([('classifier' , RandomForestClassifier())])
+param_grid = [
+    {'classifier' : [LogisticRegression()],
+     'classifier__penalty' : ['l1', 'l2'],
+    'classifier__C' : np.logspace(-4, 4, 20),
+    'classifier__solver' : ['liblinear']},
+    {'classifier' : [RandomForestClassifier()],
+    'classifier__n_estimators' : list(range(10,101,10))}
+]
 
+clf = GridSearchCV(pipe, param_grid = param_grid, cv = 5, verbose=True, n_jobs=-1)
+best_clf = clf.fit(data, y)
 
 test.drop(['PassengerId', 'Cabin','Ticket','Name'], axis=1, inplace=True)
 test = test.fillna(value = {'Age' : 23.79929,'Fare' : 1000})
 test = pd.get_dummies(test)
+test.drop(['Embarked_S','Embarked_Q','Embarked_C', 'Parch', 'Fare'], axis=1, inplace=True)
 
-y = logReg.predict(test)
+y = best_clf.predict(test)
 d = {'PassengerId':idi, 'Survived': y}
 answer = pd.DataFrame(data=d)
-np.savetxt(r'C:/Users/lysyi/Desktop/titanic/result.txt',answer,fmt='%d,%d')
+np.savetxt(r'C:/Users/lysyi/Desktop/titanic/t.txt',answer,fmt='%d,%d')
+
