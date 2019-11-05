5604adaa09a2da9b06160c28a084b0b656d9b480
Pasha <54781534+PashaLysyi321@users.noreply.github.com>
2019-11-05 20:14:27 +0200

    commit 2 (acc 0.77511)
    with using RandomForestClassifier and GridSearchCV

--- kaggle.py
+++ kaggle.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 import numpy as np
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from scipy.stats import randint as sp_randint
 
 data = pd.read_csv('C:/Users/lysyi/Desktop/titanic/train.csv', sep=',')
 test = pd.read_csv('C:/Users/lysyi/Desktop/titanic/test.csv', sep=',')
@@ -18,26 +21,26 @@ data.drop(['Survived'], axis=1, inplace=True)
 data = pd.get_dummies(data)
 data.drop(['Embarked_S','Embarked_Q','Embarked_C', 'Parch', 'Fare'], axis=1, inplace=True)
 
-pipe = Pipeline([('classifier' , RandomForestClassifier())])
-param_grid = [
-    {'classifier' : [LogisticRegression()],
-     'classifier__penalty' : ['l1', 'l2'],
-    'classifier__C' : np.logspace(-4, 4, 20),
-    'classifier__solver' : ['liblinear']},
-    {'classifier' : [RandomForestClassifier()],
-    'classifier__n_estimators' : list(range(10,101,10))}
-]
 
-clf = GridSearchCV(pipe, param_grid = param_grid, cv = 5, verbose=True, n_jobs=-1)
-best_clf = clf.fit(data, y)
+xg = RandomForestClassifier(max_features= 'sqrt')
+param_grid = {
+    'n_estimators': [150,100],
+    'max_depth':[2,3,None],
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'bootstrap': [True, False],
+    'criterion': ["gini", "entropy"]}
+
+random_search = GridSearchCV(xg, param_grid=param_grid, cv=5, iid=False)
+
+model = random_search.fit(data, y)
 
 test.drop(['PassengerId', 'Cabin','Ticket','Name'], axis=1, inplace=True)
 test = test.fillna(value = {'Age' : 23.79929,'Fare' : 1000})
 test = pd.get_dummies(test)
 test.drop(['Embarked_S','Embarked_Q','Embarked_C', 'Parch', 'Fare'], axis=1, inplace=True)
 
-y = best_clf.predict(test)
+y = model.predict(test)
 d = {'PassengerId':idi, 'Survived': y}
 answer = pd.DataFrame(data=d)
-np.savetxt(r'C:/Users/lysyi/Desktop/titanic/t.txt',answer,fmt='%d,%d')
+np.savetxt(r'C:/Users/lysyi/Desktop/titanic/result.txt',answer,fmt='%d,%d')
 
